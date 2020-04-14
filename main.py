8183897a5ed6550a37542a8e72ecee28d28d8011
uripeled2 <57635279+uripeled2@users.noreply.github.com>
2020-04-14 23:36:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+from game import Game
 from snake import *
+
 import pygame
 import random
 
+
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((WIDTH, LENGTH))
 pygame.display.set_caption("First Game")
 
 run = True
 
-cs1 = Snake(250, 250, math.pi / 4, (255, 0, 0))
-cs2 = Snake(400, 200, math.pi / 2, (0, 255, 0))
+cs1 = Snake(random.randrange(10, 490), random.randrange(10, 490), math.pi / random.randrange(1, 5), (255, 0, 0))
+cs2 = Snake(random.randrange(10, 490), random.randrange(10, 490), math.pi / random.randrange(1, 5), (0, 255, 0))
 lst_of_snakes = [cs1, cs2]
+pause = False
+history_game = [[0] * WIDTH for i in range(LENGTH)]
+history_game = cs1.add(history_game)
+history_game = cs2.add(history_game)
 
-# dict: key = pos_x; value = dict: key = pos_y; value = True;
-count = 0
+game = Game(lst_of_snakes, history_game, win)
 
 while run:
     win.fill((0, 0, 0))
@@ -24,82 +30,25 @@ while run:
             run = False
 
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        pause = not pause
 
-    if keys[pygame.K_LEFT]:
-        lst_of_snakes[0].direct -= CHENGEANGEL
-    if keys[pygame.K_RIGHT]:
-        lst_of_snakes[0].direct += CHENGEANGEL
+    if not pause:
+        # change angles
+        if keys[pygame.K_LEFT]:
+            lst_of_snakes[0].direct -= CHENGEANGEL
+        if keys[pygame.K_RIGHT]:
+            lst_of_snakes[0].direct += CHENGEANGEL
+        if keys[pygame.K_a]:
+            lst_of_snakes[1].direct -= CHENGEANGEL
+        if keys[pygame.K_s]:
+            lst_of_snakes[1].direct += CHENGEANGEL
 
-    if keys[pygame.K_a]:
-        lst_of_snakes[1].direct -= CHENGEANGEL
-    if keys[pygame.K_s]:
-        lst_of_snakes[1].direct += CHENGEANGEL
+        if not game.update():
+            run = False
+            break
 
-    # stop if anyone is die
-    # die = False
-    # for s in lst_of_snakes:
-    #     if s.is_dead:
-    #         pygame.time.delay(1000)
-    #         pygame.quit()
-
-    # stop if evrey one are dead
-    die = True
-    for s in lst_of_snakes:
-        if not s.is_dead:
-            die = False
-    if die:
-        pygame.time.delay(1000)
-        pygame.quit()
-
-
-    # move the snakes
-    for s in lst_of_snakes:
-        if s.is_dead:
-            continue
-
-        if s.is_hop:
-            s.when_to_stop -= 1
-            if s.when_to_stop <= 0:
-                s.is_hop = False
-                s.when_to_hop = random.choice(WIATUNTILHOP)
-        elif s.when_to_hop <= 0:
-            s.is_hop = True
-            s.when_to_stop = HOPINGTIME
-        else:
-            s.when_to_hop -= 1
-
-        s.last = s.int_pos
-        s.move()
-
-    # draw the snakes
-    for s in lst_of_snakes:
-        s.draw(win)
-
-    # find collision
-    for s1 in lst_of_snakes:
-        if s1.is_dead:
-            continue
-        if s1.is_hop:
-            s1.outside()
-            continue
-        if s1.collision(s1):
-            continue
-        s1.outside()
-        for s2 in lst_of_snakes:
-            if s1.is_dead:
-                break
-            if s1 == s2:
-                continue
-            s1.collision(s2)
-
-    for s1 in lst_of_snakes:
-        if s1.is_hop or s1.is_dead:
-            continue
-        elif s1.history.get(s1.int_pos[0]) is None:
-            s1.history[s1.int_pos[0]] = {}
-
-        s1.history[s1.int_pos[0]][s1.int_pos[1]] = True
-
-    pygame.display.update()
+        pygame.display.update()
 
 pygame.quit()
+
