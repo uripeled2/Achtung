0c33506fbb4812b4b1440195cd74d529f0ec94f3
Uri Peled <57635279+uripeled2@users.noreply.github.com>
2020-04-28 20:57:25 +0300

    Update state_size
    Now can deal with more state_size options

--- trainDQN.py
+++ trainDQN.py
@@ -2,6 +2,9 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense
+# from tensorflow.keras.optimizers import Adam
 from collections import deque
 import random
 from game import Game
@@ -9,10 +12,13 @@ from snake import *
 import math
 from tensorflow.keras.models import model_from_json
 import pygame
+import time
+import datetime
+import matplotlib.pyplot as plt
 
 
 class DQNAgent:
-    def __init__(self, state_size=(WIDTH, LENGTH), action_size=3):
+    def __init__(self, state_size: tuple = (WIDTH, LENGTH), action_size: int = 3):
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
@@ -22,22 +28,40 @@ class DQNAgent:
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
         self.model = self._build_model()
+        self.frame_to_pass = 2 * RADUIS
+
+    def reshape(self, lst, size: int):
+        if len(self.state_size) == 2:
+            return np.reshape(lst, (size, self.state_size[0], self.state_size[1]))
+        elif len(self.state_size) == 1:
+            return np.reshape(lst, (size, self.state_size[0]))
+        else:
+            raise Exception("Not vaild state_size")
 
     def _build_model(self):
+        ##### MY MODEL #####
         model = keras.Sequential([
-            keras.layers.Flatten(input_shape=(WIDTH, LENGTH)),
+            keras.layers.Flatten(input_shape=self.state_size),  # (state_size,) ???
             keras.layers.Dense(256, activation='relu'),
             keras.layers.Dense(64, activation='relu'),
             keras.layers.Dense(self.action_size, activation="softmax")
         ])
+
         model.compile(optimizer='adam',
                       loss='mse',
                       metrics=['accuracy'])
+        ##### COPY MODEL ####
+        # model = Sequential()
+        # model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        # model.add(Dense(24, activation='relu'))
+        # model.add(Dense(self.action_size, activation='linear'))
+        # model.compile(loss='mse',
+        #               optimizer=Adam(lr=self.learning_rate))
+        #####################
         return model
 
     def act(self, state) -> int:
         """
-
         :param state:
         :return: action: 0 or 1 or 2
         """
@@ -59,27 +83,34 @@ class DQNAgent:
             x_batch.append(state[0])
             y_batch.append(y_target[0])
 
-        # self.model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
-        x_batch = np.reshape(x_batch, [len(x_batch), WIDTH, LENGTH])
+        x_batch = self.reshape(x_batch, len(x_batch))
         y_batch = np.reshape(y_batch, [len(y_batch), 3])
         self.model.fit(x_batch, np.array(y_batch))
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
 
+def avg(lst: list) -> float:
+    return sum(lst) / len(lst)
+
+
 if __name__ == "__main__":
     best = 0
     history_game = [[0] * WIDTH for i in range(LENGTH)]
     game = Game([Snake(100, 100, math.pi / 2, (0, 255, 0))], history_game)
-    agent = DQNAgent(game)
-    episodes = 25_000
-    display_each = 3000
+    agent = DQNAgent()
+    episodes = 300
+    display_each = 20
+    alert_before = 10
+    times = []
+    scores = []
     # Iterate the game
     for e in range(episodes):
-        # print("start e: ")
+        start = time.time()
+
         # reset state in the beginning of each game
         state = game.reset()
-        state = np.reshape(state, [1, WIDTH, LENGTH])
+        state = agent.reshape(state, 1)
 
         # PyGame set win
         if e % display_each == 0:
@@ -88,12 +119,13 @@ if __name__ == "__main__":
             pygame.display.set_caption(F"{e}")
         else:
             win = None
+        if e + alert_before % display_each == 0:
+            # TODO Set alert using noise
+            pass
         game.win = win
-        
         # time_t represents each frame of the game
-        # Our goal is to keep the pole upright as long as possible until score of 500
         # the more time_t the more score
-        for time_t in range(500):
+        for time_t in range(200):
             # render
             if win is not None:
                 win.fill((0, 0, 0))
@@ -102,7 +134,8 @@ if __name__ == "__main__":
                 pygame.display.update()
 
             # Decide action
-            action = agent.act(state)
+            if time_t % agent.frame_to_pass == 0:
+                action = agent.act(state)
 
             # Advance the game to the next frame based on the action.
             game.lst_of_snakes[0].change_angle(action)
@@ -117,7 +150,7 @@ if __name__ == "__main__":
             else:
                 reward = -200
             _ = None
-            next_state = np.reshape(next_state, [1, WIDTH, LENGTH])
+            next_state = agent.reshape(next_state, 1)
 
             # memorize the previous state, action, reward, and done
             agent.memorize(state, action, reward, next_state, done)
@@ -126,19 +159,25 @@ if __name__ == "__main__":
             state = next_state
 
             # done becomes True when the game ends
-            # ex) The agent drops the pole
             if done:
+                end = time.time()
+                took = end - start
+                times.append(took)
+                scores.append(time_t)
                 if time_t > best:
                     best = time_t
                 # print the score and break out of the loop
-                print(F"episode: {e}/{episodes} score: {time_t} best: {best}")
+                print(F"episode: {e}/{episodes} score: {time_t} best: {best} took: {round(took, 2)} "
+                      F"average time: {round(avg(times), 2)}")
                 break
 
         # close win
         pygame.quit()
 
         # train the agent with the experience of the episode
-        agent.replay(32)
+        agent.replay(64)
+
+    print(F"total time: {sum(times)}")
 
     # serialize model to JSON
     model_json = agent.model.to_json()
@@ -157,5 +196,7 @@ if __name__ == "__main__":
     loaded_model.load_weights("model.h5")
     print("Loaded model from disk")
 
-
-
+    # plot
+    plt.plot(scores)
+    plt.ylabel('scores')
+    plt.show()
